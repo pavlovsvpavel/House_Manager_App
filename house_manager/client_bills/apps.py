a9203e60896941365665b6cc186229b24e221bd5
Pavel Pavlov <pavelsvpavlov@gmail.com>
2024-02-29 08:47:10 +0200

    Initial commit

---
+++ house_manager/client_bills/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ClientBillsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'house_manager.client_bills'
---
+++ house_manager/house_bills/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HouseBillsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'house_manager.house_bills'
