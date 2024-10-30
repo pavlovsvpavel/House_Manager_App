07d8df4c817ab30fbdd6f99dcf3160e96b218ab6
Pavel Pavlov <pavelsvpavlov@gmail.com>
2024-10-30 22:06:40 +0200

    Update signature field behavior and fixes in reports views.

--- staticfiles/scripts/signature.js
+++ staticfiles/scripts/signature.js
@@ -1,26 +1,35 @@
 $(document).ready(function () {
     // Initialize the signature widget
-    $('#signature').jSignature();
+    $('#signature').jSignature({
+        width: "100%",
+        height: 400,
 
-    if ($('#id_is_paid').is(':checked')) {
-            $('#signature').show();  // Show signature pad if checked
+    });
+
+    function toggleSignaturePad() {
+        if ($('#id_is_paid').is(':checked')) {
+            $('#signature').show();
+            // Smooth scroll to the signature pad
+            $('html, body').animate({
+                scrollTop: $('#signature').offset().top - 50
+            }, 600);
         } else {
-            $('#signature').hide();  // Hide signature pad if not checked
+            $('#signature').hide();
         }
-    //
-    $('#id_is_paid').change(function() {
-            if ($(this).is(':checked')) {
-                $('#signature').show();
-            } else {
-                $('#signature').hide();
-            }
-        });
+    }
+
+    // Check initial state on page load
+    toggleSignaturePad();
+
+    // Scroll and toggle signature pad visibility on checkbox change
+    $('#id_is_paid').change(function () {
+        toggleSignaturePad();
+    });
 
     const emptySignature = $('#signature').jSignature('getData', 'svgbase64')[1];
     // Capture the signature data on form submission
     $('form.is-paid-form').submit(function (event) {
         const data = $('#signature').jSignature('getData', 'svgbase64');
-        // $('#signature_input').val(data[1]);
         if (data[1] === emptySignature) {
             $('#signature_input').val('');  // Set to empty string if no signature
         } else {
