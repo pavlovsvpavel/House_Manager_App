66c71e5764ea7f4c5a81c1b680c750ecfcf1a2be
Pavel Pavlov <pavelsvpavlov@gmail.com>
2024-11-06 10:10:28 +0200

    Revert "Renamed fee_manager_and_cashier field to fee_manager. Added fee_cashier field."
    This reverts commit b79bb52ceff1e45e39405d50b7dfe1594a3180d0.

--- house_manager/common/mixins.py
+++ house_manager/common/mixins.py
@@ -125,7 +125,7 @@ class MonthlyBill(models.Model):
         verbose_name=_("Cleaner fee"),
     )
 
-    fee_manager = models.DecimalField(
+    fee_manager_and_cashier = models.DecimalField(
         max_digits=MAX_DECIMAL_DIGITS,
         decimal_places=MAX_DECIMAL_PLACES,
         blank=False,
@@ -133,18 +133,7 @@ class MonthlyBill(models.Model):
         validators=(
             MinValueValidator(MIN_VALUE, message=_("Please enter a value greater than zero")),
         ),
-        verbose_name=_("Manager fee"),
-    )
-
-    fee_cashier = models.DecimalField(
-        max_digits=MAX_DECIMAL_DIGITS,
-        decimal_places=MAX_DECIMAL_PLACES,
-        blank=False,
-        null=False,
-        validators=(
-            MinValueValidator(MIN_VALUE, message=_("Please enter a value greater than zero")),
-        ),
-        verbose_name=_("Cashier fee"),
+        verbose_name=_("Manager/Cashier fee"),
     )
 
     repairs = models.DecimalField(
