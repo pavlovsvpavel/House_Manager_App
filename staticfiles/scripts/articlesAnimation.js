559ea3837231c178d75d7ed46b17d027b2775c18
Pavel Pavlov <pavelsvpavlov@gmail.com>
2024-12-21 13:07:40 +0200

    Updates in UI design.

---
+++ staticfiles/scripts/articlesAnimation.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const articles = document.querySelectorAll("#articles-section article");
+
+    const observer = new IntersectionObserver(
+        (entries, observer) => {
+            entries.forEach((entry, index) => {
+                if (entry.isIntersecting) {
+                    setTimeout(() => {
+                        entry.target.classList.add("article-visible");
+                        observer.unobserve(entry.target);
+                    }, index * 300);
+                }
+            });
+        },
+        {
+            root: null,
+            rootMargin: "0px",
+            threshold: 0.1
+        }
+    );
+
+    articles.forEach((article) => {
+        observer.observe(article);
+    });
+});
+
